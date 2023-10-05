1b5505a88859d2f8d8469c8df42b73274bab2c5d
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-10-05 19:22:24 +0530

    Changed MenuItemCard

--- components/MenuItemCard.jsx
+++ components/MenuItemCard.jsx
@@ -17,10 +17,17 @@ const MenuItemCard = ({ name, price, desc }) => {
           </p>
         </CardTitle>
       </CardHeader>
-      <CardContent className="p-4 pt-2 flex flex-1 flex-col gap-3 text-lg max-sm:text-sm font-semibold text-gray-800">
+      <CardContent className="p-4 pt-2 flex flex-1 flex-row gap-3 text-lg max-sm:text-sm font-semibold text-gray-800">
         <p className="font-hind text-gray-800 tracking-[0.06rem]">{desc}</p>
-        <p>₹ {price}</p>
       </CardContent>
+      <div className="flex flex-1 items-center justify-between relative">
+        <p className="bg-red-300 rounded-tr-lg font-bold p-2 w-24 font-hind text-gray-900 tracking-[0.06rem]">
+          ₹ {price}
+        </p>
+        <button className="rounded-tl-lg bg-[#C8F169]  font-hind text-gray-900 tracking-[0.06rem] font-bold ring-0 border-0 p-2">
+          Add to Card
+        </button>
+      </div>
     </Card>
   );
 };
