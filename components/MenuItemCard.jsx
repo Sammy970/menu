b5ac24f314ec0c2fe4c49ab0dcd785086a730587
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-10-05 19:28:06 +0530

    Hopefull fixed - MenuItemCard

--- components/MenuItemCard.jsx
+++ components/MenuItemCard.jsx
@@ -20,11 +20,11 @@ const MenuItemCard = ({ name, price, desc }) => {
       <CardContent className="p-4 pt-2 flex flex-1 flex-row gap-3 text-lg max-sm:text-sm font-semibold text-gray-800">
         <p className="font-hind text-gray-800 tracking-[0.06rem]">{desc}</p>
       </CardContent>
-      <div className="flex flex-1 items-center justify-between relative">
-        <p className="bg-red-300 rounded-tr-lg font-bold p-2 w-24 font-hind text-gray-900 tracking-[0.06rem]">
+      <div className="flex flex-1 items-start justify-between">
+        <p className="bg-red-300 rounded-tr-lg  font-bold p-2 font-hind text-gray-900 tracking-[0.06rem]">
           ₹ {price}
         </p>
-        <button className="rounded-tl-lg bg-[#C8F169]  font-hind text-gray-900 tracking-[0.06rem] font-bold ring-0 border-0 p-2">
+        <button className="rounded-tl-lg bg-[#C8F169] font-hind text-gray-900 tracking-[0.06rem] font-bold ring-0 border-0 p-2">
           Add to Card
         </button>
       </div>
