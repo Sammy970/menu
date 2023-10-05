8aff2209ab0d183c6455f89c49e4543c4956dd49
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-10-05 17:46:24 +0530

    Fixed Everything by moving sections into APP Folder

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import MenuSection from "@/sections/MenuSection";
+import MenuSection from "@/app/sections/MenuSection";
 import Nav from "@/components/Nav";
-import Table from "@/sections/Table";
+import Table from "@/app/sections/Table";
 
 export default function Home() {
   return (
@@ -11,16 +11,6 @@ export default function Home() {
       </section>
       <section className="mt-10">
         <MenuSection />
-        <div className="grid grid-cols-2 gap-3 mb-10 mt-6">
-          <div>
-            <p className="mt-4 max-sm:grid-cols-1 text-gray-50">Dining out just got better</p>
-          </div>
-          <div>2</div>
-          <div>3</div>
-          <div>5</div>
-          <div>4</div>
-          <div>5</div>
-        </div>
       </section>
       <section></section>
       <section></section>
