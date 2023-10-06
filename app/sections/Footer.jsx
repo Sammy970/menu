962f055c197113b81a470018b72f43b682485107
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-10-06 12:19:36 +0530

    Changed Footer - Powered by MENU CSS

--- app/sections/Footer.jsx
+++ app/sections/Footer.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="max-container mt-16 mb-8">
+    <footer className="max-container mt-16 mb-8 selection">
       <hr className="h-px my-8 bg-[#C8F169] border-0 dark:bg-gray-700" />
       <div className="flex flex-1 justify-start items-center">
         <Image
@@ -25,9 +25,12 @@ const Footer = () => {
 
       <div className="flex flex-1 flex-col mt-5">
         <h2 className="font-hind text-gray-300 text-lg font-normal leading-normal tracking-[0.1rem]">
-          Powered by MENU
+          Powered by{" "}
+          <span className="inline-block bg-[#C8F169] text-black font-khand font-extrabold transform -skew-y-12 px-2">
+            MENU
+          </span>
         </h2>
-        <h2 className="font-khand text-2xl tracking-[0.12rem] mt-2 text-white">
+        <h2 className="font-khand text-2xl tracking-[0.12rem] mt-3 text-white">
           Give your restaurant
         </h2>
         <h1 className="font-khand text-3xl tracking-[0.15rem] leading-none mt-1 text-[#C8F169]">
