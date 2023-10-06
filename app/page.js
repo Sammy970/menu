c5c5a16df7999bcdd1a54d1b8ce8718b43808e12
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-10-06 11:28:02 +0530

    Added Footer Section for both Mobile and Desktop Devices

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import MenuSection from "@/app/sections/MenuSection";
 import Nav from "@/components/Nav";
 import Table from "@/app/sections/Table";
+import Footer from "@/app/sections/Footer";
 
 export default function Home() {
   return (
@@ -12,7 +13,9 @@ export default function Home() {
       <section className="mt-10">
         <MenuSection />
       </section>
-      <section></section>
+      <section>
+        <Footer />
+      </section>
       <section></section>
       <section></section>
     </main>
---
+++ app/sections/Footer.jsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+const Footer = () => {
+  return (
+    <footer className="max-container mt-16 mb-8">
+      <hr className="h-px my-8 bg-[#C8F169] border-0 dark:bg-gray-700" />
+      <div className="flex flex-1 justify-start items-center">
+        <Image
+          src="/Assets/coco_logo.png"
+          width={70}
+          height={70}
+          alt="logo"
+          className="p-2 rounded-full bg-[#EEF2E3] border-black border-2"
+        />
+      </div>
+      <div className="flex flex-1 flex-col mt-5">
+        <h2 className="font-khand text-white text-3xl tracking-[0.12rem]">
+          Coco's Bar/Kitchen
+        </h2>
+        <p className="font-hind text-gray-300 text-lg max-sm:text-sm mt-3 tracking-[0.12rem] leading-normal font-semibold">
+          State and Government Taxes applicable <br /> We levy 7.5% Service
+          Charge
+        </p>
+      </div>
+
+      <div className="flex flex-1 flex-col mt-5">
+        <h2 className="font-hind text-gray-300 text-lg font-normal leading-normal tracking-[0.1rem]">
+          Powered by MENU
+        </h2>
+        <h2 className="font-khand text-2xl tracking-[0.12rem] mt-2 text-white">
+          Give your restaurant
+        </h2>
+        <h1 className="font-khand text-3xl tracking-[0.15rem] leading-none mt-1 text-[#C8F169]">
+          An Unfair Advantage
+        </h1>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
