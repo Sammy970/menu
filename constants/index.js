df3ae8a91eababe76cca9cfc7e8dc56b8d4666ff
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-10-06 14:47:40 +0530

    Added constants - Beverages and Food in Constants

--- constants/index.js
+++ constants/index.js
@@ -19,8 +19,93 @@ export const itemMenu = [
               // Add more cocktails here
             ],
           },
+          {
+            "Wine & Bubbly Cocktails": [
+              {
+                name: "Sangria Bianco",
+                price: "INR 425",
+                desc: "White Wine, Apple, Bianco Vermouth, Cheery Nut cordial",
+              },
+              {
+                name: "Melon Mimosa",
+                price: "INR 425",
+                desc: "Sparkling Wine, Japanese Green Melon, Citrus Juices",
+              },
+            ],
+          },
+          {
+            "Classics - Reimagined": [
+              {
+                name: "Clover Club",
+                price: "INR 500",
+                desc: "Gin - Raspberry - Citrus - Vegan Foam",
+              },
+              {
+                name: "Gin Basil Smash",
+                price: "INR 500",
+                desc: "Gin - Basil - Citrus",
+              },
+            ],
+          },
 
           // Add more subcategories here
+        ],
+      },
+      {
+        "Zero Alcohol Cocktails": [
+          {
+            "Zero Alcohol Cocktails": [
+              {
+                name: "Ichigo",
+                price: "INR 225",
+                desc: "Berry Compote, Passion Fruit, Cranberry Juice, Black Tea, Citrus, Sparkling Water",
+              },
+              {
+                name: "Summer Bloom",
+                price: "INR 250",
+                desc: "Italian Red Bitter Liqueur (0.0%). Pink Grapefruit Cordial, OJ, Sea Salt, Sparkling Water",
+              },
+              {
+                name: "Not A Colada",
+                price: "INR 225",
+                desc: "Pineapple, Coconut Tart, Vegan Foam, Tropical Bitters",
+              },
+            ],
+          },
+          {
+            Bubblys: [
+              {
+                name: "Yuzu Bubbly",
+                price: "INR 125",
+                desc: "Yuzu - Soda",
+              },
+              {
+                name: "Pink Grapefruit Bubbly",
+                price: "INR 150",
+                desc: "Pink Grapefruit - Soda",
+              },
+              {
+                name: "Kafir Lime Bubbly",
+                price: "INR 150",
+                desc: "Kafir Lime - Soda",
+              },
+            ],
+          },
+          {
+            "Zero Alcohol Spirits & Liqueurs": [],
+          },
+          {
+            "Fresh Juices": [
+              {
+                name: "Watermelon Juice",
+                price: "INR 125",
+              },
+              {
+                name: "Tender Coconut Water",
+                price: "INR 125",
+              },
+            ],
+          },
         ],
       },
       // Add more items here
@@ -29,6 +114,29 @@ export const itemMenu = [
   {
     Food: [
       // Define food items similarly to beverages
+      {
+        Starters: [
+          {
+            "Quickies | Nibbles": [
+              {
+                name: "Peanut Masala",
+                price: "INR 150",
+                desc: "Boiled peanuts with green chili paste and assorted greens",
+              },
+              {
+                name: "The Cubbon Park Chaat",
+                price: "INR 120",
+                desc: "Pineapple and sautekaai, chunks with a zesty chilli mix and Congress peanuts",
+              },
+              {
+                name: "CCCP sticks",
+                price: "INR 170",
+                desc: "The good old classic with Cocos twist, cheese cheery chilly pineapple sticks",
+              },
+            ],
+          },
+        ],
+      },
     ],
   },
   {
